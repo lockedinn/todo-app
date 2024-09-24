7abf79ba7012f661761b4aff2abc4c96afe48ad8
lockedinn <batu73580@gmail.com>
2024-09-24 23:52:45 +0400

    added style and added some functions in main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const taskInput = document.querySelector(".task-input");
-const taskAdd = document.querySelector(".add-task-btn");
 const taskList = document.querySelector(".task-list");
 
-taskAdd.addEventListener("click", function () {
+let taskArr = [];
+
+const addTask = () => {
   const newInput = taskInput.value;
 
   if (newInput.trim() === "") {
@@ -12,7 +13,41 @@ taskAdd.addEventListener("click", function () {
 
   const newTask = document.createElement("li");
   newTask.textContent = newInput;
-  taskList.appendChild(newTask);
+
+  const removeBtn = document.createElement("button");
+  removeBtn.textContent = "Remove Task";
+  removeBtn.classList.add("remove-btn");
+
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+
+  taskArr.push({ task: newInput, completed: false });
 
   taskInput.value = "";
-});
+
+  newTask.appendChild(removeBtn);
+  newTask.appendChild(checkBox);
+  taskList.appendChild(newTask);
+
+  checkBox.addEventListener("click", () => {
+    if (checkBox.checked) {
+      newTask.style.color = "red";
+    } else {
+      newTask.style.color = "black";
+    }
+  });
+
+  removeBtn.addEventListener("click", () => {
+    taskList.removeChild(newTask);
+
+    const taskIndex = taskArr.indexOf(newInput);
+    if (taskIndex > -1) {
+      taskArr.splice(taskIndex, 1);
+    }
+  });
+
+  console.log(taskArr);
+};
+
+
+document.querySelector(".add-task-btn").addEventListener("click", addTask);
